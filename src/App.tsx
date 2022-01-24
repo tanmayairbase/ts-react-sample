eec220688fb28eda552d1d4ff3427124725c726f
tanmay <tanmay.rajani@airbase.io>
2022-01-24 13:04:24 +0530

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    setLoading(true);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
+        {loading ? "loading" : ""}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
